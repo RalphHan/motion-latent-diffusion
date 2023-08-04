a3bc31d356e494cea54d077bcd94a5bfc6c28b36
RalphHan <hanhongwei_hf@qq.com>
2023-08-04 13:14:41 +0800

    add relational anim

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ from mld.data.get_data import get_datasets
 from mld.models.get_model import get_model
 from mld.utils.logger import create_logger
 
+from mld.data.humanml.utils.plot_script import plot_3d_motion
+
 
 def init():
     sys.argv.extend("--cfg ./configs/config_mld_humanml3d.yaml --cfg_assets ./configs/assets.yaml".split())
@@ -40,38 +42,42 @@ def init():
     model.to(device)
     model.eval()
 
-    return cfg, model, logger
+    return cfg, model
 
 
-def action(render, prompt, cfg, model, logger):
+def action(render, prompt, cfg, model):
     the_uuid = str(uuid.uuid4())
+    fps = 20
     video_name = f"{cfg.gradio_dir}/{the_uuid}.mp4"
     json_name = f"{cfg.gradio_dir}/{the_uuid}.json"
 
     with torch.no_grad():
         batch = {"length": [100], "text": [prompt]}
-        joints, quaternion, r_pos = model(batch, return_quaternion=True)
-    joints = joints[0].numpy()[..., [2, 0, 1]]
-    quaternion = quaternion[0].numpy()
-    if joints.shape[-2] == 22:
-        joints = np.concatenate([joints, joints[..., -2:, :]], axis=-2)
-        quaternion = np.concatenate([quaternion, np.zeros_like(quaternion[..., -2:, :])], axis=-2)
-    r_pos = r_pos[0].numpy()
+        joints = model(batch)
+    joints = joints[0].numpy()
     with open(json_name, "w") as f:
-        json.dump({"root_positions": r_pos.tolist(),
-                   "rotations": quaternion.tolist(),
-                   "fps": 20}, f, indent=4)
-    if render:
-        skeleton_render(joints, video_name)
+        json.dump({"positions": joints.flatten().tolist(),
+                   "fps": fps,
+                   "mode": "xyz",
+                   "n_frames": joints.shape[0],
+                   "n_joints": 22}, f, indent=4)
+    if render != "none":
+        if render == "absolute":
+            skeleton_render(np.concatenate((joints, joints[:, -2:]), axis=-2)[..., [2, 0, 1]], video_name)
+        elif render == "relational":
+            plot_3d_motion(video_name, joints * 1.3, radius=3, title=prompt, fps=fps)
+        else:
+            raise ValueError(f"render {render} not supported")
         return video_name, json_name
     return None, json_name
 
 
 if __name__ == "__main__":
-    cfg, model, logger = init()
+    cfg, model = init()
     demo = gr.Interface(
-        lambda render, prompt: action(render, prompt, cfg, model, logger),
-        [gr.Checkbox(value=True, label="render"), gr.Textbox("A person is skipping rope.")],
+        lambda render, prompt: action(render, prompt, cfg, model),
+        [gr.Dropdown(choices=["none", "relational", "absolute"], value="relational", label="render"),
+         gr.Textbox("A person is skipping rope.")],
         [gr.Video(format="mp4", autoplay=True), gr.File()],
     )
     demo.launch(server_name='0.0.0.0', server_port=7867)

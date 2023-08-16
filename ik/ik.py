9e305e95537ccf3c7e58a91ce8f0455228aa1cdd
RalphHan <hanhongwei_hf@qq.com>
2023-08-16 16:27:53 +0800

    add ml based ik

--- ik/ik.py
+++ ik/ik.py
@@ -1,8 +1,8 @@
 from .quaternion import *
 
 offsets = np.array([[0, 0, 0],
-                    [1, 0, 0],
-                    [-1, 0, 0],
+                    [0.7071, -0.7071, 0],
+                    [-0.7071, -0.7071, 0],
                     [0, 1, 0],
                     [0, -1, 0],
                     [0, -1, 0],
@@ -13,8 +13,8 @@ offsets = np.array([[0, 0, 0],
                     [0, 0, 1],
                     [0, 0, 1],
                     [0, 1, 0],
-                    [1, 0, 0],
-                    [-1, 0, 0],
+                    [0.7071, 0.7071, 0],
+                    [-0.7071, 0.7071, 0],
                     [0, 0, 1],
                     [0, -1, 0],
                     [0, -1, 0],
@@ -50,24 +50,47 @@ parents = [
 ]
 
 
-def get_rotation_naive(sons, joints):
+def get_rotation_naive(sons, weights, joints):
     ret = np.zeros(joints.shape[:-2] + (4,))
-    for j in sons[1:]:
+    for j, w in zip(sons[1:], weights[1:]):
         u = offsets[j][np.newaxis, ...].repeat(len(joints), axis=0)
         v = joints[:, j] - joints[:, sons[0]]
         v = v / np.sqrt((v ** 2).sum(axis=-1))[:, np.newaxis]
-        ret += qbetween_np(u, v)
-    # ret /= len(sons) - 1
+        ret += w * qbetween_np(u, v)
     ret /= np.sqrt((ret ** 2).sum(axis=-1, keepdims=True))
     return ret
+
+
+def get_rotation(sons, weights, joints):
+    num_iters = 50
+    rotation = torch.tensor(get_rotation_naive(sons, weights, joints), requires_grad=True, device='cpu',
+                            dtype=torch.float32)
+    optim = torch.optim.LBFGS([rotation], lr=1e-2, max_iter=num_iters, line_search_fn='strong_wolfe')
+    for i in range(num_iters):
+        def closure():
+            optim.zero_grad()
+            offset_ = torch.tensor(offsets[sons[1:], np.newaxis].repeat(len(joints), axis=1).reshape(-1, 3)).to(
+                rotation).detach()
+            v_ = qrot((rotation / torch.sqrt((rotation ** 2).sum(-1, keepdims=True))).tile([len(sons) - 1, 1]), offset_)
+            v = joints[:, sons[1:]] - joints[:, sons[0:1]]
+            v = v / np.sqrt((v ** 2).sum(axis=-1, keepdims=True))
+            v = torch.tensor(v.transpose(1, 0, 2).reshape(-1, 3)).to(rotation).detach()
+            w = torch.tensor(weights[1:]).to(rotation).detach()
+            loss = (w * ((v - v_) ** 2).reshape(len(sons) - 1, -1).mean(-1)).sum() / w.sum()
+            loss.backward()
+            return loss
+
+        optim.step(closure)
+    rotation = (rotation / torch.sqrt((rotation ** 2).sum(-1, keepdims=True))).detach().cpu().numpy()
+    return rotation
 
 
 def ik(joints):
     roots = []
     sons = [[0, 1, 2, 3], [9, 12, 13, 14]]
+    weights = [[0, 1, 1, 1], [0, 1, 1, 1]]
     for i in range(len(sons)):
-        roots.append(get_rotation_naive(sons[i], joints))
-
+        roots.append(get_rotation_naive(sons[i], weights[i], joints))
     quat_params = np.zeros(joints.shape[:-1] + (4,))
     quat_params[..., 0] = 1
     quat_params[:, 0] = roots[0]
@@ -81,10 +104,10 @@ def ik(joints):
                 continue
             if chain[j] == 9: continue
             u = offsets[chain[j + 1]][np.newaxis, ...].repeat(len(joints), axis=0)
-            u_=qrot_np(roots[0] if chain[j]<12 else roots[1], u)
+            u_ = qrot_np(roots[0] if chain[j] < 12 else roots[1], u)
             v = joints[:, chain[j + 1]] - joints[:, chain[j]]
             v = v / np.sqrt((v ** 2).sum(axis=-1))[:, np.newaxis]
-            rot_u_v = qmul_np(roots[0] if chain[j]<12 else roots[1], qbetween_np(u_, v))
+            rot_u_v = qmul_np(roots[0] if chain[j] < 12 else roots[1], qbetween_np(u_, v))
             quat_params[:, chain[j]] = qmul_np(qinv_np(R), rot_u_v)
             R = qmul_np(R, quat_params[:, chain[j]])
 
@@ -144,6 +167,7 @@ def fk(rotations, root_positions):
 if __name__ == '__main__':
     import json
     import binascii
+
     with open("playground/f5ebdd69-6bc5-493b-b808-72ef24b72916.json") as f:
         data = json.load(f)
     joints = np.frombuffer(binascii.a2b_base64(data["positions"]), dtype=data["dtype"]).reshape(data["n_frames"],

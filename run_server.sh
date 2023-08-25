949697891479bc16ac4fd9bf2376da5640884848
RalphHan <hanhongwei_hf@qq.com>
2023-08-25 17:47:04 +0800

    support docker

--- run_server.sh
+++ run_server.sh
@@ -1,4 +1,4 @@
 cd /motion-latent-diffusion
-rm deps;ln -s /data/deps .
-rm models;ln -s /data/models .
+rm deps;ln -s /deps .
+rm models;ln -s /models .
 /opt/conda/envs/mld/bin/uvicorn main:app --host 0.0.0.0 --port 8019 --workers 2
